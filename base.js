4c20fb2c1d809a42f8843bad499c1bb4c91d64dc
nathanallen <thenathanator@gmail.com>
2014-11-15 23:12:12 -0800

    random block background colors

--- base.js
+++ base.js
@@ -16,6 +16,21 @@ function check_for_winner(){
   }
 }
 
+function random_color(){
+  var hex = ["#"]
+  while (hex.length < 7) {
+    hex.push( Math.floor(Math.random() * 16).toString(16) )
+  }
+  return hex.join('')
+}
+
+function randomize_block_colors(el){
+  var n = el.length
+  for (var i = 0; i < n; i++) {
+    $(el[i]).css('background-color', random_color())
+  }
+}
+
 function build_gameboard(columns,blocks){
   for (var i = 0; i < columns; i++) {
     $('body#app').append("<div class='column'></div>")
@@ -25,12 +40,14 @@ function build_gameboard(columns,blocks){
     $('div.column').append("<div class='block'></div>")
   }
 
+  randomize_block_colors($('div.block'))
+
   $('div.block').on('mouseleave', hide_element)
   $('div.block').on('click', stick_element)
 }
 
 $(document).ready(function(){
 
-  build_gameboard(10,5)
+  build_gameboard(10,50)
 
 });
